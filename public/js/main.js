025802c4a9401d1d9bc88263afe3a3661debcc5b
alondramora <mora_ac@yahoo.com>
2023-07-27 08:55:54 -0700

    added a fetch that gets me the google maps location of one of my documents - will be refactored to be async await

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
 console.log("javascript is working");
+
+//fetches the googleMapLocation from a specific document in my collection
+fetch("http://localhost:3001/spaces/64bf43a0a6e214f3435df46f")
+  .then((response) => response.json())
+  .then((location) => {
+    document.getElementById("googleMapsLocationImage").src =
+      location.googleMapLocation;
+  });
