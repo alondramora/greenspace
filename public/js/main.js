da3005669cd8b8deee8c815fec602aeb87e20d28
alondramora <mora_ac@yahoo.com>
2023-07-28 20:56:08 -0700

    created the storeSpacesInArrayAndAddtoDom function that currently takes the names of the documents and stores them in an array

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,51 @@
-console.log("javascript is working");
+// const { response } = require("express");
+
+//stores the array of my green spaces in the greenSpaceCollection variable
+async function storeSpacesInArrayAndAddtoDom() {
+  //stores the array in a variable
+  const greenSpaceCollection = await fetch(
+    "http://localhost:3001/getAllSpaces"
+  );
+  // ensures we get the JSON from the fetch promise
+  const greenSpaceDocumentJson = await greenSpaceCollection.json();
+  // storing the names of the documents in an array called greenSpaceNames
+  const greenSpaceNames = greenSpaceDocumentJson.map((x) => {
+    // create an h2 for each document that grabs the document.name
+    return x.name;
+  });
+  //logs the names of all the documents in the array
+  console.log(greenSpaceNames);
+}
+storeSpacesInArrayAndAddtoDom();
+
+//   //for each document in the array, I want to create an h2 with the document.name and an img tag with the document.googleMapLocation as the src
+
+///////////////////////
+// async function fetchGoogleMapsLocationImage(id) {
+//   try {
+//     const response = await fetch(`http://localhost:3001/spaces/${id}`);
+//     if (!response.ok) {
+//       // if the response is not ok
+//       throw new Error("Network response not ok.");
+//     }
+//     // if the response is ok then...
+//     const location = await response.json();
+//     document.getElementById("googleMapsLocationImage").src =
+//       location.googleMapLocation; // grab the img tag with the id of "googleMapsLocationImage" and update the src so that it links to the googleMapLocation (url) field of the document
+//   } catch (error) {
+//     console.error("Error fetching Google Map location:", error);
+//     throw error;
+//   }
+// }
+
+// window.addEventListener("DOMContentLoaded", async () => {
+//   try {
+//     await fetchGoogleMapsLocationImage("64bf43a0a6e214f3435df46f");
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// });
+//////////////////////////
 
 //fetches the googleMapLocation from a specific document in my collection
 // fetch("http://localhost:3001/spaces/64bf43a0a6e214f3435df46f")
@@ -7,28 +54,4 @@ console.log("javascript is working");
 //     document.getElementById("googleMapsLocationImage").src =
 //       location.googleMapLocation;
 //   });
-
-async function fetchGoogleMapsLocationImage(id) {
-  try {
-    const response = await fetch(`http://localhost:3001/spaces/${id}`);
-    if (!response.ok) {
-      // if the response is not ok
-      throw new Error("Network response not ok.");
-    }
-    // if the response is ok then...
-    const location = await response.json();
-    document.getElementById("googleMapsLocationImage").src =
-      location.googleMapLocation; // grab the img tag with the id of "googleMapsLocationImage" and update the src so that it links to the googleMapLocation (url) field of the document
-  } catch (error) {
-    console.error("Error fetching Google Map location:", error);
-    throw error;
-  }
-}
-
-window.addEventListener("DOMContentLoaded", async () => {
-  try {
-    await fetchGoogleMapsLocationImage("64bf43a0a6e214f3435df46f");
-  } catch (error) {
-    console.error("Error:", error);
-  }
-});
+////////////////////////
