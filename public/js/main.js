e642871c2b3fe48d62a7001519cced7f8b621617
alondramora <mora_ac@yahoo.com>
2023-07-31 12:58:01 -0700

    figured out how to extract the google images from each document, now I just need to figure out how to display the SpaceImages and the SpaceNames together

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,23 @@
 // const { response } = require("express");
 
-//stores the array of my green spaces in the greenSpaceCollection variable
-async function storeSpacesInArrayAndAddtoDom() {
+//Goal: List out the names of each space, followed by a google maps image location
+async function listSpacesAndImages() {
   // code will only run if we are on the /spaces page
   if (window.location.pathname == "/spaces") {
-    //stores the array in a variable
+    //stores the array in a variable greenSpaceCollection
     const greenSpaceCollection = await fetch(
       "http://localhost:3001/getAllSpaces"
     );
-    // ensures we get the JSON from the fetch promise
+    // ensures we get the JSON from the promise
     const greenSpaceDocumentJson = await greenSpaceCollection.json();
-    // storing the names of the documents in an array called greenSpaceNames
-    const greenSpaceNames = await greenSpaceDocumentJson.map((x) => {
-      // create an h2 for each document that grabs the document.name
+
+    const SpaceNames = await greenSpaceDocumentJson.map((x) => {
+      // stores the names all documents in the collection in an array called SpaceNames
       return x.name;
     });
 
-    // spaceNamesH2 takes each element in the greenSpaceNames array and creates an h2 element in the html of the spaces page for each one, they each have a class name of space-names
-    let spaceNamesH2 = greenSpaceNames.map((x) => {
+    // Takes each element in the SpaceNames array and creates an h2 element in the html for each one, they each have a class name of space-name
+    SpaceNames.map((x) => {
       const createH2 = document.createElement("h2");
       createH2.classList.add("space-name");
       createH2.innerHTML = x;
@@ -25,38 +25,19 @@ async function storeSpacesInArrayAndAddtoDom() {
       console.log(`${x} now has an h2!`);
     });
 
-    // const greenSpaceImages = await greenSpaceDocumentJson.map((x) => {
-    //   return x.googleMapLocation;
-    // });
+    // I also want to add in images for each green space
+    const SpaceImages = await greenSpaceDocumentJson.map((x) => {
+      // stores all of the googleMapsLocation values into one array called SpaceImages
+      return x.googleMapLocation;
+    });
+
+    SpaceImages.map((x) => {
+      const createImg = document.createElement("img");
+      createImg.classList.add("location-image");
+      createImg.src = x;
+      document.body.appendChild(createImg);
+      console.log("Image added");
+    });
   }
 }
-storeSpacesInArrayAndAddtoDom();
-
-//   //for each document in the array, I want to create an h2 with the document.name and an img tag with the document.googleMapLocation as the src
-
-///////////////////////
-// async function fetchGoogleMapsLocationImage(id) {
-//   try {
-//     const response = await fetch(`http://localhost:3001/spaces/${id}`);
-//     if (!response.ok) {
-//       // if the response is not ok
-//       throw new Error("Network response not ok.");
-//     }
-//     // if the response is ok then...
-//     const location = await response.json();
-//     document.getElementById("googleMapsLocationImage").src =
-//       location.googleMapLocation; // grab the img tag with the id of "googleMapsLocationImage" and update the src so that it links to the googleMapLocation (url) field of the document
-//   } catch (error) {
-//     console.error("Error fetching Google Map location:", error);
-//     throw error;
-//   }
-// }
-
-// window.addEventListener("DOMContentLoaded", async () => {
-//   try {
-//     await fetchGoogleMapsLocationImage("64bf43a0a6e214f3435df46f");
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// });
-//////////////////////////
+listSpacesAndImages();
