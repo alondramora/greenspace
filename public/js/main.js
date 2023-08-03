bb508873e9e65a31393deae35813dd6855734329
alondramora <mora_ac@yahoo.com>
2023-08-02 20:05:02 -0700

    used a map array method to create and h2 and img element for each document in my spaces collection, need to add css for UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,34 @@
 // const { response } = require("express");
 
-//Goal: List out the names of each space, followed by a google maps image location
+//Goal: List out the names of each space, followed by a google maps image
 async function listSpacesAndImages() {
   // code will only run if we are on the /spaces page
   if (window.location.pathname == "/spaces") {
-    //stores the array in a variable greenSpaceCollection
+    //fetch the green space collection obj and store it in a variable
     const greenSpaceCollection = await fetch(
       "http://localhost:3001/getAllSpaces"
     );
-    // ensures we get the JSON from the promise
+    // gets the JSON from the promise we fetched and stores it in variable
+    // greenSpaceDocumentJson is an array of objects, each object is the document from our collection
     const greenSpaceDocumentJson = await greenSpaceCollection.json();
+    // console.log(greenSpaceDocumentJson);
 
-    const SpaceNames = await greenSpaceDocumentJson.map((x) => {
-      // stores the names all documents in the collection in an array called SpaceNames
-      return x.name;
-    });
-
-    // Takes each element in the SpaceNames array and creates an h2 element in the html for each one, they each have a class name of space-name
-    SpaceNames.map((x) => {
+    // map through the array of document objects - 'x' is each object which contains an entire document
+    greenSpaceDocumentJson.map((x) => {
+      const spaceName = x.name; // gets the name of document
+      const spaceLocationTest = x.googleMapLocation; // gets img url
+      // create h2s for every spaceName
       const createH2 = document.createElement("h2");
       createH2.classList.add("space-name");
-      createH2.innerHTML = x;
+      createH2.innerHTML = spaceName;
       document.body.appendChild(createH2);
-      console.log(`${x} now has an h2!`);
-    });
-
-    // I also want to add in images for each green space
-    const SpaceImages = await greenSpaceDocumentJson.map((x) => {
-      // stores all of the googleMapsLocation values into one array called SpaceImages
-      return x.googleMapLocation;
-    });
-
-    SpaceImages.map((x) => {
+      console.log(`${spaceName} now has an h2!`);
+      // creates an img element
       const createImg = document.createElement("img");
       createImg.classList.add("location-image");
-      createImg.src = x;
-      document.body.appendChild(createImg);
+      createImg.src = spaceLocationTest;
+      // append the new img element to the h2 we created for this document
+      createH2.appendChild(createImg);
       console.log("Image added");
     });
   }
